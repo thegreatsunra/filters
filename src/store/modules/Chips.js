14d0b6103d1a8103e8fae894d95e18798fe98d4c
Dane Petersen <thegreatsunra@gmail.com>
2018-05-16 22:12:16 -0500

    Add chips state for tag cloud thing

---
+++ src/store/modules/Chips.js
@@ -0,0 +1,30 @@
+const state = [
+  {
+    name: 'cpus',
+    children: []
+  },
+  {
+    name: 'motherboards',
+    children: []
+  },
+  {
+    name: 'storage',
+    children: []
+  }
+]
+
+const mutations = {
+}
+
+const actions = {
+}
+
+const getters = {
+}
+
+export default {
+  actions,
+  getters,
+  mutations,
+  state
+}
